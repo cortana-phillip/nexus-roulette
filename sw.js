cce517455d09e3945d164a6b08d4e4f32d2e0c29
cortana-phillip <cortana.rajcany@gmail.com>
2026-04-04 00:56:12 -0700

    Fix: cur not defined in AddTrackPanel

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nexus-roulette-v9';
+const CACHE = 'nexus-roulette-v10';
 const FILES = ['./','./index.html','./manifest.json','./icon.svg'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
