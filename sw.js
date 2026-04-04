f4859adb2858740cfb233c467f0603fe97b80ede
Cortana <cortana@nexus.app>
2026-04-04 17:54:14 +0000

    Remove Gravity Roulette from simulation mode

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nexus-roulette-v25';
+const CACHE = 'nexus-roulette-v26';
 const FILES = ['./','./index.html','./manifest.json','./icon.svg'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
