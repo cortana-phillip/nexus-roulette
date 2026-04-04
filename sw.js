5b12691f09641504acf64f58f7c1398e3af43c7b
Cortana <cortana@nexus.app>
2026-04-04 20:22:46 +0000

    Fix: spinNum crash, FTL+drought independent toggles, turning on clears static targets

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nexus-roulette-v34';
+const CACHE = 'nexus-roulette-v35';
 const FILES = ['./','./index.html','./manifest.json','./icon.svg'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
