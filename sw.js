6ac51014dabd6e86e431fdd7c0b95d3bfb9acb43
Cortana <cortana@nexus.app>
2026-04-04 19:46:01 +0000

    Fix: bankroll tappable for exact input

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nexus-roulette-v31';
+const CACHE = 'nexus-roulette-v32';
 const FILES = ['./','./index.html','./manifest.json','./icon.svg'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
