4551cf82f1c4acfe5d72535e8b770194727255e0
cortana-phillip <cortana.rajcany@gmail.com>
2026-04-03 22:34:08 -0700

    Fix: bankroll, colors, vibration, tab persistence

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nexus-roulette-v2';
+const CACHE = 'nexus-roulette-v3';
 const FILES = ['./','./index.html','./manifest.json','./icon.svg'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
